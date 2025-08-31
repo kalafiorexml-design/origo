96d792fdb784107b5baeada682e7c3dd8a4d8452
kalafiorexml-design <kalafiorexml@gmail.com>
2025-08-31 03:30:49 +0200

    Update build_site.js

--- scripts/build_site.js
+++ scripts/build_site.js
@@ -1,1 +1,7 @@
+const fs = require('fs');
+const data = JSON.parse(fs.readFileSync('website/top5.json','utf8'));
+let html = fs.readFileSync('website/index.html','utf8');
+html = html.replace(/const global=\[[\s\S]*?\];/, `const global=${JSON.stringify(data.global)};`)
+           .replace(/const personal=\[[\s\S]*?\];/, `const personal=${JSON.stringify(data.personal)};`);
+fs.writeFileSync('website/index.html', html);
 
